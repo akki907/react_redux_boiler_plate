cc08042a2cf81d1d5c583e987af61ef3259c09d1
akki907 <ak.akki907@gmail.com>
2019-11-16 10:20:36 +0530

    initil commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Router>
+            <div >Boiler Plate</div>
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
